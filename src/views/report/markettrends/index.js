3289746be52565ba349b4b6b020d99b5ce584e8c
theMoon0777 <sama950729@gmail.com>
2023-09-02 21:39:07 +0900

    market trends: first version responsive

--- src/views/report/markettrends/index.js
+++ src/views/report/markettrends/index.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
+
+import logo from "./logo.png";
 
 //redux part
 import { useDispatch, useSelector } from 'react-redux';
@@ -9,19 +10,24 @@ import { setValue } from '../../../store/slice/homeslice';
 import './index.css';
 
 import { AccordionComponent, AccordionItemsDirective, AccordionItemDirective } from '@syncfusion/ej2-react-navigations';
-import { Button, Typography } from '@mui/material';
-// import { PlunkMarketInsightChart } from './plunkMarektInsightChart';
-import { HomeCompareChart } from './homeCompareChart';
-import { PlunkReModelValueChart } from './plunkRemodelValueChart';
+// import { HomeCompareChart } from './homeCompareChart';
 import { useEffect } from 'react';
 import axios from 'axios';
+import { LineChart } from '@mui/x-charts/LineChart';
+import { HomeCompareChart } from './homeCompareChart';
 
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
+const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
+const xLabels = [
+  'Page A',
+  'Page B',
+  'Page C',
+  'Page D',
+  'Page E',
+  'Page F',
+  'Page G',
+];
+
 
 const PlunkHomeValue = () => {
   // const [homeprice ,setHomeprice] = useState("");
@@ -45,7 +51,7 @@ const PlunkHomeValue = () => {
     axios.post('http://localhost:7000/api/home-value', addressObj).then((res) => {
       dispatch(setValue(res.data));
     });
-   
+
   }, [dispatch]);
 
   useEffect(() => {
@@ -55,60 +61,53 @@ const PlunkHomeValue = () => {
 
 
   return (
-    <Box
-      sx={{
-        borderRadius: 8,
-        padding: 10,
-        backgroundColor: 'white',
-        boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.15)'
-      }}
-    >
-      <Box
-        sx={{
-          color: 'black',
-          fontSize: '20px',
-          marginBottom: '50px'
-        }}
-      >
+
+    <div className="plunk-comp">
+      <div className='plunk-comp-header'>
         Plunk Home Value™
-      </Box>
-      <Box
-        sx={{
-          textAlign: 'center'
-        }}
-      >
-        <Box
-          id="numberpart1"
-          sx={{
-            color: 'black',
-            fontSize: '30px',
-            marginBottom: '15px'
-          }}
-        >
-          $417722.54
-        </Box>
-        <Box
-          sx={{
-            color: 'green',
-            fontSize: '20px',
-            marginBottom: '20px'
-          }}
-        >
-          $299.62/day
-        </Box>
-        <Button
-          variant="contained"
-          sx={{
-            marginBottom: '50px'
-          }}
-        >
-          Improve accuracy
-        </Button>
-        <Box>powered by plunk</Box>
-      </Box>
-    </Box>
+      </div>
+      <div className="plunk-comp-des">
+        Delivers real-time valuation for 104+ million homes nationwide
+      </div>
+      <div className="plunk-comp-main lx lx-col align-center">
+        <div className="lx space-between w-100">
+          <span className="plunk-value-title">Plunk Home Value™</span>
+          <div>
+            <img alt="Free Cycle Refresh Turn Icon" loading="lazy" src="https://cdn.iconscout.com/icon/free/png-256/free-refresh-1754122-1493235.png?f=webp&amp;w=128" srcSet="https://cdn.iconscout.com/icon/free/png-256/free-refresh-1754122-1493235.png?f=webp&amp;w=128 1x, https://cdn.iconscout.com/icon/free/png-256/free-refresh-1754122-1493235.png?f=webp&amp;w=256 2x" className="thumb_p6OvR" />
+          </div>
+        </div>
+        <div className="color-spec font-weight-normal value-price-big">
+          $992,4469.
+          <span className="value-price-small">03</span>
+        </div>
+        <div>
+          <span></span>
+          <span className="color-green font-weight-normal value-day">$299.62/day</span>
+        </div>
+        <div>
+          <button className="accuracy-btn bgcolor-lightblue">Improve accuracy -</button>
+        </div>
+        <div>
+          <span className="small-size-span">powered by</span>
+          <span><img className="small-logo" src={logo} alt="loading" /> </span>
+        </div>
+      </div>
+      <div>
+        <button className="big-accuracy-btn">Plunk Home Value™ API</button>
+      </div>
+    </div>
   );
 };
+const plunkParent = () => {
+  return (
+    <div className="plunk-parent">
+      <PlunkHomeValue />
+      <PlunkMarketInsight />
+      <PlunkHomeCompare />
+      <PlunkRemodelValue />
+    </div>
+  )
+}
 const PlunkMarketInsight = () => {
   // const dispatch = useDispatch();
 
@@ -127,68 +126,48 @@ const PlunkMarketInsight = () => {
   // }, [insights])
 
   return (
-    <Box
-      sx={{
-        borderRadius: 8,
-        padding: 10,
-        backgroundColor: 'white',
-        boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.15)'
-      }}
-    >
-      <Box
-        sx={{
-          color: 'black',
-          fontSize: '20px'
-        }}
-      >
+    <div className="plunk-comp">
+      <div className='plunk-comp-header color-lightspec'>
         Plunk Market Insights™
-      </Box>
-      <Box
-        sx={{
-          color: 'black',
-          fontSize: '15px',
-          marginBottom: '50px'
-        }}
-      >
-        for 98115 - August 2nd, 2023
-      </Box>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} id="fixed-high-table" aria-label="simple table">
-          <TableBody>
-            {/* {
-            !insightarr? "" : insightarr.map((data, key) => (
-              <TableRow
-                key={key}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                >
-                <TableCell align="left">
-                  <p className="mb-xs">{data.date}</p>
-                  <p className="mb-xs font-size-xs">Valuation</p>
-                </TableCell>
-                <TableCell align="left">
-                  <p className="mb-xs">${data.value}</p>
-                  <p className="mb-xs font-size-xs">Min</p>
-                </TableCell>
-            </TableRow>
-            ))
-            } */}
-            <TableRow
-              key="0" sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                >
-              <TableCell  scope="row">
-                <p className="mb-xs">2023-09-01</p>
-                <p className="mb-xs font-size-xs">Valuation</p>
-              </TableCell>
-              <TableCell className="hidden-th" align="left">
-                <p className="mb-xs">365</p>
-                <p className="mb-xs font-size-xs">Min</p>
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
-      {/* <PlunkMarketInsightChart id="insig"/> */}
-    </Box>
+      </div>
+      <div className="plunk-comp-des market-insight-des">
+        Tracks ZIP code-level market insights through key housing metrics
+      </div>
+      <div className="plunk-comp-main lx lx-col align-center h-335">
+        <div className="lx space-between w-100 lx-col">
+          <span className="font-size-normal">Plunk Market Insights™</span>
+          <span className="font-size-s">for 98115 - August 2nd , 2023</span>
+        </div>
+        <div className="chart-container">
+          <div className="lx">
+            <span>Sale vs. list price</span>
+            <span>!</span>
+          </div>
+          <div className="lx">
+            <span>o</span>
+            <span>Actual</span>
+            <div></div>
+            <span>90day average</span>
+          </div>
+          <div>
+            <span>103.21%</span>
+            <LineChart
+              width={500}
+              height={300}
+              series={[
+                { data: pData, label: 'pv' },
+                { data: uData, label: 'uv' },
+              ]}
+              xAxis={[{ scaleType: 'point', data: xLabels }]}
+            />
+          </div>
+
+        </div>
+      </div>
+      <div>
+        <button className="big-accuracy-btn font-size-22 market-insight-btn">Plunk Market Insights™ API</button>
+      </div>
+    </div>
   );
 };
 const PlunkHomeCompare = () => {
@@ -213,149 +192,95 @@ const PlunkHomeCompare = () => {
   // }, [dispatch]);
 
   return (
-    <Box
-      sx={{
-        borderRadius: 8,
-        padding: 10,
-        backgroundColor: 'white',
-        boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.15)'
-      }}
-    >
-      <Box
-        sx={{
-          color: 'black',
-          fontSize: '20px'
-        }}
-      >
-        Home compared to 91436
-      </Box>
-      <Box
-        sx={{
-          color: 'black',
-          fontSize: '15px',
-          marginBottom: '50px'
-        }}
-      >
-        June 19th, 2023
-      </Box>
-      <HomeCompareChart />
-    </Box>
+    <div className="plunk-comp">
+      <div className='plunk-comp-header color-lightspec'>
+        Home Compare
+      </div>
+      <div className="plunk-comp-des">
+        Provides statitic on how a home compares against others in the neightborhood
+      </div>
+      <div className="plunk-comp-main lx lx-col align-center">
+        <div className='lx lx-col'>
+          <span>Home COmparred to 91436</span>
+          <span>September 2th, 2023</span>
+        </div>
+        <div className='lx'>
+          <div className='lx'>
+            <div className="check-div bg-gray"></div>
+            <span>In 91436</span>
+          </div>
+          <div>
+            <div className="check-div bg-blue"></div>
+            <span>this home</span>
+          </div>
+        </div>
+        <div>Numbers Of Bathrooms</div>
+        <div>
+          <HomeCompareChart />
+        </div>
+      </div>
+      <div>
+        <button className="big-accuracy-btn">Home Compare API</button>
+      </div>
+    </div>
   );
 };
 const PlunkRemodelValue = () => {
   return (
-    <Box
-      id="plunk-remodal"
-      sx={{
-        borderRadius: 8,
-        padding: 10,
-        backgroundColor: 'white',
-        boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.15)'
-      }}
-    >
-      <Box
-        sx={{
-          color: 'black',
-          fontSize: '20px',
-          marginBottom: '50px'
-        }}
-      >
+    <div className="plunk-comp">
+      <div className='plunk-comp-header color-lightspec'>
         Plunk Remodel Value™
-      </Box>
-      <Box
-        id="plunk-remodal-container"
-        sx={{
-          display: 'flex',
-          justifyContent: 'space-between'
-        }}
-      >
-        <Box
-          sx={{
-            textAlign: 'right',
-            width: '40%',
-            marginRight: '20px'
-          }}
-        >
-          <Box
-            sx={{
-              backgroundColor: 'green',
-              borderRadius: 8,
-              padding: '10px',
-              marginBottom: '30px'
-            }}
-          >
-            <Typography
-              sx={{
-                color: 'black',
-                fontSize: '20px'
-              }}
-            >
-              Plunk Remodel Value™
-            </Typography>
-            <Typography
-              id="numberpart"
-              sx={{
-                color: 'black',
-                fontSize: '30px'
-              }}
-            >
-              $2,267,790
-            </Typography>
-            <Typography
-              sx={{
-                color: 'green',
-                fontSize: '15px'
-              }}
-            >
-              $299.62 /day
-            </Typography>
-          </Box>
-          <Typography
-            sx={{
-              color: 'gray',
-              fontSize: '15px'
-            }}
-          >
-            Remodel up
-          </Typography>
-          <Typography
-            sx={{
-              color: 'black',
-              fontSize: '15px',
-              marginBottom: '40px'
-            }}
-          >
-            $821,090
-          </Typography>
-          <Typography
-            sx={{
-              color: 'gray',
-              fontSize: '15px'
-            }}
-          >
-            Plunk Home Value™
-          </Typography>
-          <Typography
-            sx={{
-              color: 'black',
-              fontSize: '15px',
-              marginBottom: '40px'
-            }}
-          >
-            $1,446,700
-          </Typography>
+      </div>
+      <div className="plunk-comp-des remodel-des">
+        Introduces a new metric that uncovers a homes upside or potential value with certain upgrades
+      </div>
+      <div className="plunk-comp-main lx lx-col align-center">
+        <div className="lx space-between w-100">
+          <span className="font-size-normal">Plunk Remodel Value™</span>
+        </div>
+        <div className='lx'>
+          <div className='lx lx-col lx-2'>
+            <div className="remodel-comp lx lx-col lx-2">
+              <span>Plunk Remodel Value™</span>
+              <span className='remodel-comp-price'>$22,675,3</span>
+              <span>$299.63/day</span>
+            </div>
+            <span className='bar-next-div-span right'>
+              -Remodel upside
+              <span className='bar-next-div-span'>&821,090</span>
+            </span>
+            <span className='bar-next-div-span right'>-Plunk Home Value™<span className='bar-next-div-span'>$1,446,700</span></span>
+          </div>
+          <div>
+            <div className='lx lx-col lx-1 align-center'>
+              <div className='remodel-bar-parent'>
+              </div>
+              <div className='remodel-bar-part'>
+              </div>
+              <div className='remodel-bar-part'>
+              </div>
+              <div className='remodel-bar-part'>
+              </div>
+              <div className='remodel-bar-parent'></div>
+            </div>
+          </div>
 
-          <Box>powered by plunk</Box>
-        </Box>
-        <Box
-          sx={{
-            width: '40%'
-          }}
-        >
-          <PlunkReModelValueChart />
-        </Box>
-      </Box>
-    </Box>
+          <div className="lx lx-2 lx-col bar-next-div">
+            <span className="bar-next-div-span">- Expaned first floor</span>
+            <span className="bar-next-div-span">- Remodel kitchen to luxury grade</span>
+            <span className="bar-next-div-span">- Add a full bath(+sqf)</span>
+            <span className="bar-next-div-span">- +2more projects</span>
+          </div>
+        </div>
+        <div>
+          <span className="small-size-span">powered by</span>
+          <span><img className="small-logo" src={logo} alt="loading" /> </span>
+        </div>
+      </div>
+      <div>
+        <button className="big-accuracy-btn remodel-btn">Plunk Remodel Value API</button>
+      </div>
+    </div>
   );
 };
 
@@ -374,7 +299,7 @@ export const MarketTrends = () => {
       <div className="permit-accordian-div" id="item-14-1">
         <AccordionComponent>
           <AccordionItemsDirective>
-            <AccordionItemDirective header="Plunk Home Value™" content={PlunkHomeValue} />
+            <AccordionItemDirective header="Plunk Home Value™" id="plunks-container" content={plunkParent} />
           </AccordionItemsDirective>
         </AccordionComponent>
       </div>
